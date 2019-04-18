59bfc75ee83d00dbc125bc9643347b90bd3ecd3f
nyx-coder <kadamshailesh007@gmail.com>
2019-04-19 00:56:25 +0530

    added animation to the screens

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -2,6 +2,31 @@ import {createStackNavigator,createAppContainer} from "react-navigation"
 import MainScreen from "../screens/MainScreen"
 import NewsScreen from "../screens/NewsScreen"
 import CategoryScreen from "../screens/CategoryScreen"
+import { fromLeft, zoomIn, zoomOut } from 'react-navigation-transitions'
+ 
+const handleCustomTransition = ({ scenes }) => {
+  const prevScene = scenes[scenes.length - 2];
+  const nextScene = scenes[scenes.length - 1];
+ 
+  // Custom transitions go there
+  if (prevScene
+    && prevScene.route.routeName === 'Home'
+    && nextScene.route.routeName === 'Category') {
+    return fromLeft(1000);
+  } else if (prevScene
+    && prevScene.route.routeName === 'Category'
+    && nextScene.route.routeName === 'Home') {
+    return fromRight(1000);
+  } else if (prevScene
+    && prevScene.route.routeName === 'Home'
+    && nextScene.route.routeName === 'News') {
+    return zoomIn(500);
+  } else if (prevScene
+    && prevScene.route.routeName === 'News'
+    && nextScene.route.routeName === 'Home') {
+    return zoomOut(500);
+  }
+}
 
 const navigation = createStackNavigator({
   Home: {
@@ -22,6 +47,8 @@ const navigation = createStackNavigator({
         header: null
       }
     },
+},{
+  transitionConfig: (nav) => handleCustomTransition(nav)
 })
 
 const Navigation = createAppContainer(navigation);
